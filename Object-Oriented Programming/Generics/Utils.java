db096cc4ec0c7ee8564d82f7246cf74300038374
Logan Stoltz <102991130+LoganStoltz@users.noreply.github.com>
2025-09-25 18:33:09 -0700

    Create Utils.java

---
+++ Object-Oriented Programming/Generics/Utils.java
@@ -0,0 +1,11 @@
+public class Utils {
+
+    // GENERIC METHOD EXAMPLE
+    // Generic method to print elements of an array
+    public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+}
